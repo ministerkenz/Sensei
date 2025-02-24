937188a30b1b32f52bee2b91e5103c90ff0560b6
Kenzo Yubitani <kommoken@gmail.com>
2025-02-24 12:54:56 -0500

    Initial Commit

---
+++ Sensei/SenseiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SenseiApp.swift
+//  Sensei
+//
+//  Created by Kenzo Yubitani (student LM) on 2/24/25.
+//
+
+import SwiftUI
+
+@main
+struct SenseiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
