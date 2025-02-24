4bc4884d467ea2d86a020ff594b6edd707443389
Aarav Gupta <s024506@students.lmsd.org>
2025-02-24 12:54:56 -0500

    Initial Commit

---
+++ Sensei/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sensei
+//
+//  Created by Kenzo Yubitani (student LM) on 2/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, aarav!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
